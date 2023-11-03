ab60d8c7df0f65504e17fd2d7ee9e65f71bd3aca
metecan <metecankaplan@hotmail.com>
2023-11-03 15:34:54 +0300

    [FEAT]: Add useFetchWeather hook for fetching weather data

---
+++ src/app/hooks/useFetchWeather.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import useSWR from 'swr';
+
+const fetcher = (url: string) => axios.get(url).then(res => res.data);
+
+export const useFetchWeather = (lat: string, lon: string) => {
+  const { data, isLoading } = useSWR(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=daily,minutely,hourly,alerts&appid=${process.env.NEXT_PUBLIC_API_KEY}&units=metric`,
+    fetcher,
+  );
+  return { data, isLoading };
+};
